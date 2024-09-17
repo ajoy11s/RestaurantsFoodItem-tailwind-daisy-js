32eae5b89f02dd45ff1691133995b69be66dae6f
ajoy11s <ajoycse0811@gmail.com>
2024-09-17 17:36:01 +0600

    commit change

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ function CallFoodData(searchTerm) {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
       .then(response => response.json())
       .then(data => {
-        //var count = Object.keys(data.meals).length;
+        const data_Object = localStorage.getItem("data_Object") || 0;
+        localStorage.setItem("data_Object", data.meals);
         let text = " <div class='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12'>";
         for (let x in data.meals) {
-          text += "<div class='card bg-base-100 w-96 md:w-96 lg:w-72 h-96 md:h-96 lg:h-400px shadow-xl'>";
+          text += "<div class='card bg-base-100 w-80 md:w-96 lg:w-72 h-96 md:h-96 lg:h-400px shadow-xl'>";
           text += "<figure class='px-10 pt-10'><img src='" + data.meals[x]['strMealThumb'] + "' alt='Shoes'class='rounded-xl'  /></figure>";
           text += "<div class='card-body items-left text-left'><h3 class='card-title'>" + data.meals[x]['strMeal'].substr(0, 20) + "..." + "</h3>";
           text += "<p>" + data.meals[x]['strInstructions'].substr(0, 50) + "..." + "</p>";
           text += "<div class='card-actions justify-end'><button class='btn btn-info' onclick='my_modal_3.showModal()' id='btn'+ x+'details'>Details</button> </div>";
-          text += "</div></div>";
-          console.log('btn'+ x+'details');
+          text += "</div></div>";         
+          //console.log('btn'+ x+'details');
           document.getElementById("modal_meal").innerHTML = "<b>Meal:</b><i> " +data.meals[x]['strMeal']+"</i>";
           document.getElementById("modal_category").innerHTML = "<b>Category:</b><i> " +data.meals[x]['strCategory']+"</i>";
           document.getElementById("modal_area").innerHTML = "<b>Area:</b><i> " +data.meals[x]['strArea']+"</i>";
@@ -21,6 +22,12 @@ function CallFoodData(searchTerm) {
         }
         text += "</div>"
         document.getElementById("container").innerHTML = text;
+        //const myJSON = JSON.stringify(data_Object);
+        var data = JSON.parse(JSON.stringify(data_Object));
+        console.log(data);
+       
+        console.log('typeof retrievedObject: ' + typeof data_Object);
+        console.log('Value of retrievedObject: ' + data_Object);
 
       }
       )
