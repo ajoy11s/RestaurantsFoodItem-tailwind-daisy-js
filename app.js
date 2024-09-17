85fa503c6027b6105ed5173ff8aacc253181270e
ajoy11s <ajoycse0811@gmail.com>
2024-09-17 17:51:16 +0600

    commit change

--- app.js
+++ app.js
@@ -4,15 +4,16 @@ function CallFoodData(searchTerm) {
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
       .then(response => response.json())
       .then(data => {
+        //var count = Object.keys(data.meals).length;
         let text = " <div class='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12'>";
         for (let x in data.meals) {
-          text += "<div class='card bg-base-100 w-80 md:w-96 lg:w-72 h-96 md:h-96 lg:h-400px shadow-xl'>";
+          text += "<div class='card bg-base-100 w-96 md:w-96 lg:w-72 h-96 md:h-96 lg:h-400px shadow-xl'>";
           text += "<figure class='px-10 pt-10'><img src='" + data.meals[x]['strMealThumb'] + "' alt='Shoes'class='rounded-xl'  /></figure>";
           text += "<div class='card-body items-left text-left'><h3 class='card-title'>" + data.meals[x]['strMeal'].substr(0, 20) + "..." + "</h3>";
           text += "<p>" + data.meals[x]['strInstructions'].substr(0, 50) + "..." + "</p>";
           text += "<div class='card-actions justify-end'><button class='btn btn-info' onclick='my_modal_3.showModal()' id='btn'+ x+'details'>Details</button> </div>";
-          text += "</div></div>";         
-          //console.log('btn'+ x+'details');
+          text += "</div></div>";
+          console.log('btn'+ x+'details');
           document.getElementById("modal_meal").innerHTML = "<b>Meal:</b><i> " +data.meals[x]['strMeal']+"</i>";
           document.getElementById("modal_category").innerHTML = "<b>Category:</b><i> " +data.meals[x]['strCategory']+"</i>";
           document.getElementById("modal_area").innerHTML = "<b>Area:</b><i> " +data.meals[x]['strArea']+"</i>";
@@ -20,7 +21,6 @@ function CallFoodData(searchTerm) {
         }
         text += "</div>"
         document.getElementById("container").innerHTML = text;
-    
 
       }
       )
